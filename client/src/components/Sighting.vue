6d1cc9c440b4981f85e75dd0e3dbe4d700217df1
James Fraser <jimbob4t@virginmedia.com>
2020-01-21 16:01:09 +0000

    post route created and tested

---
+++ client/src/components/Sighting.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="sighting">
+    <h2>{{ sighting.species }}</h2>
+    <p>{{ sighting.location }} on {{ sighting.date|format }}</p>
+
+    <button>Delete Sighting</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sighting",
+  props: ['sighting'],
+  filters: {
+    format(value){
+      return new Date(value).toLocaleString().substring(0, 10);
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.sighting {
+	width: 30%;
+	background: rgba(255, 255, 255, 0.7);
+	margin-bottom: 20px;
+	padding: 25px;
+}
+
+button {
+	color: #fff;
+	border: none;
+	font-size: 18px;
+	padding: 10px;
+	margin-top: 10px;
+	background: #F55536;
+}
+</style>
---
+++ client/src/services/SightingService.js
@@ -0,0 +1,8 @@
+const baseURL = 'http://localhost:3000/api/sightings/'
+
+export default {
+  getSightings(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  }
+}
